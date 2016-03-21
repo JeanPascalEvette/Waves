014b550c6272298f00dd8910b77a91fb6b4de8f2
Jean-Pascal Evette(Laptop) <jp.evette1@gmail.com>
2016-03-21 14:47:53 +0000

    Added different camera views

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,17 +4,27 @@ using System.Collections;
 public class CameraController : MonoBehaviour {
 
     [SerializeField]
-    private Transform target;
+    private int currentPosition;
 
-    [SerializeField]
-    private float height;
+    private Vector3[] positionList;
+    private Quaternion[] rotationList;
 
-	// Use this for initialization
-	void Start () {
-	    
-	}
+    // Use this for initialization
+    void Start () {
+
+        positionList = new Vector3[] { transform.position, new Vector3(18245, 17684, 22194.06f), new Vector3(2167.666f, 13829.45f, 9980.756f) };
+        rotationList = new Quaternion[] { transform.rotation, Quaternion.Euler(new Vector3(82.64277f, 90.1254f, 359.9477f)), Quaternion.Euler(new Vector3(45.68707f, 52.35602f, 359.9915f)) };
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
+
+        if(Input.GetKeyDown(KeyCode.F2))
+        {
+            currentPosition = (currentPosition + 1) % positionList.Length;
+            transform.position = positionList[currentPosition];
+            transform.rotation = rotationList[currentPosition];
+        }
 	}
 }
